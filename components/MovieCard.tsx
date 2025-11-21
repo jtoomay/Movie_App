559027c3944a8edfcc3f733912a17e5c1f6e1bc1
Jake Toomay <jake@toomay.dev>
2025-11-20 23:36:38 -0600

    Home screen complete.

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
+import { icons } from "@/constants/icons";
 import { Link } from "expo-router";
-import { Image, Text, TouchableOpacity } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const MovieCard = ({
   id,
@@ -9,7 +10,7 @@ const MovieCard = ({
   release_date,
 }: Movie) => {
   return (
-    <Link href={`/movie/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
         <Image
           source={{
@@ -20,7 +21,24 @@ const MovieCard = ({
           className="w-full h-52 rounded-lg"
           resizeMode="cover"
         />
-        <Text className="text-white text-sm">{title}</Text>
+        <Text numberOfLines={1} className="text-white text-sm">
+          {title}
+        </Text>
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-xs text-white font-bold uppercase">
+            {Math.round(vote_average / 2)}
+          </Text>
+
+          <View className="flex-row items-center justify-between">
+            <Text className="text-xs text-light-300 font-medium mt-1">
+              {release_date?.split("-")[0]}
+            </Text>
+            <Text className="text-xs font-medium text-light-300 uppercase">
+              Movie
+            </Text>
+          </View>
+        </View>
       </TouchableOpacity>
     </Link>
   );
