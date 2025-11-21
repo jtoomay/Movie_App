1289af133b372d5360d82b0ac051f5a178499376
Jake Toomay <jake@toomay.dev>
2025-11-20 23:27:47 -0600

    Homepage Design Done

---
+++ services/api.ts
@@ -0,0 +1,32 @@
+export const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+console.log("🚀 ~ TMDB_CONFIG:", TMDB_CONFIG);
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  console.log("🚀 ~ fetchMovies ~ response:", response);
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+
+  const data = await response.json();
+  console.log("🚀 ~ fetchMovies ~ data:", data);
+
+  return data.results;
+};
